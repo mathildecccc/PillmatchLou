c6c741ffb70bb34f50d36b165ac4b10c2a8ad651
mathildecccc <mathilde.comptepro@gmail.com>
2025-08-08 12:19:14 +0200

    Update Header.tsx
    Lou to PillMatch

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header>
       <div className="header-content">
-        <h1>Lou</h1>
+        <h1>PillMatch</h1>
       </div>
     </header>
   );
